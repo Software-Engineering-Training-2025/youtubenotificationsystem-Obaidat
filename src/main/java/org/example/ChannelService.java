2e7fa8858022423af5d3ec17ce0677498840768a
Obaidat <ahmadaymanobaidat@gmail.com>
2025-09-13 19:23:44 +0300

    .

--- src/main/java/org/example/ChannelService.java
+++ src/main/java/org/example/ChannelService.java
@@ -1,19 +1,68 @@
 package org.example;
 
+import org.example.observer.Observer;
 import org.example.subject.Channel;
 import org.example.subject.Subject;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
 public class ChannelService {
 
-    public void createNewChannel(Channel channel) {
-        // TODO: implement
+    private final Map<String, Channel> channels = new HashMap<>();
+
+    public boolean createChannel(String name){
+
+        if (name.isBlank() || name.isEmpty() || channels.containsKey(name)){
+            return false;
+        }
+        channels.put(name, new Channel(name));
+        return true;
     }
 
-    public void subscribeToChannel(Channel channel, Subject subject) {
-        // TODO: implement
+    public boolean deleteChannel(String name){
+        if (channels.containsKey(name)){
+            channels.remove(name);
+            return true;
+        }
+        return false;
     }
 
-    public void newContentNotification(Channel channel, String message) {
-        // TODO: implement
+    public boolean subscribe(String channelName, Observer user){
+        if (channels.containsKey(channelName) && user != null){
+            channels.get(channelName).subscribe(user);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean unsubscribe(String channelName, Observer user){
+        if (channels.containsKey(channelName) && user != null){
+            channels.get(channelName).unsubscribe(user);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean upload(String channelName, String videoTitle){
+        if (channels.containsKey(channelName) && !videoTitle.isEmpty()){
+            channels.get(channelName).uploadVideo(videoTitle);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean notifyObservers(String channelName, String notification){
+        if (channels.containsKey(channelName) && !notification.isEmpty()){
+            channels.get(channelName).notifyObservers(notification);
+            return true;
+        }
+        return false;
+    }
+
+    public Set<String> listChannels(){
+        return channels.keySet();
     }
 }
--- src/main/java/org/example/subject/Channel.java
+++ src/main/java/org/example/subject/Channel.java
@@ -2,30 +2,41 @@ package org.example.subject;
 
 import org.example.observer.Observer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Channel implements Subject{
+
     private final String name;
+    private final List<Observer> subscribers = new ArrayList<>();
 
     public Channel(String name) {
         this.name = name;
     }
 
     public void uploadVideo(String title){
-        // TODO: Notify all subscribers about new video
+        this.notifyObservers(this.name + "uploaded a new video: " + title);
     }
 
     @Override
     public void subscribe(Observer observer) {
-        // TODO: Add subscriber
+        if (observer != null) {
+            subscribers.add(observer);
+        }
     }
 
     @Override
     public void unsubscribe(Observer observer) {
-        // TODO: Remove subscriber
+        if (observer != null){
+            subscribers.remove(observer);
+        }
     }
 
     @Override
     public void notifyObservers(String message) {
-        // TODO: Notify subscribers
+        for (Observer observer : subscribers){
+            observer.update(message);
+        }
     }
 
     public String getName() {
